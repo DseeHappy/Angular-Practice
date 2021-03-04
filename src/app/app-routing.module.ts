61ea8c8c33bb7ee1e3da0a60631646668342a267
Juan Daniel Sanchez <32243398+DseeHappy@users.noreply.github.com>
2021-03-04 15:28:46 -0700

    1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ErrorPageComponent } from './error-page/error-page.component';
 
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'about', component: AboutComponent},
+  {path:'not-found', component: ErrorPageComponent, data:{message: 'Page not found!'}},
+  {path:'**', redirectTo: '/not-found'}
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,36 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { AboutComponent } from './about/about.component';
+import { MdbModule } from 'mdb-angular-ui-kit';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ErrorPageComponent } from './error-page/error-page.component';
+import { MainCtaComponent } from './home/main-cta/main-cta.component';
+import { SectionOneComponent } from './home/section-one/section-one.component';
+import { SectionTwoComponent } from './home/section-two/section-two.component';
+import { MdbCollapseModule } from 'mdb-angular-ui-kit';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    FooterComponent,
+    AboutComponent,
+    ErrorPageComponent,
+    MainCtaComponent,
+    SectionOneComponent,
+    SectionTwoComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MdbModule,
+    MdbCollapseModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
